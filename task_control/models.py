45d8ee169f69980908bb3bed1ba978962e9bb9bd
mikola-s <suharev.nv@gmail.com>
2020-02-04 17:47:31 +0200

    task_control/models.py add Status & TasksModel

--- task_control/models.py
+++ task_control/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    order = models.PositiveIntegerField()
+    name = models.CharField(max_length=20)
+
+
+class TaskModel(models.Model):
+    title = models.CharField(max_length=50)
+    creator = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, default=4, related_name='creator')
+    worker = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True, related_name='worker')
+    status = models.ForeignKey(to=Status, on_delete=models.SET_DEFAULT, default=1)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"PK {self.pk} OWNER {self.owner.username} TITLE {self.title} STATUS {self.status.name}"
